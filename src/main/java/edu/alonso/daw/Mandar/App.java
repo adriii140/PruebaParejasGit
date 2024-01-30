fb4636da53f84cb820bbca976eed45db23a4e514
adriii140 <adrian.sanmor@educa.jcyl.es>
2024-01-30 09:46:18 +0100

    Se crea maven project y se manda

---
+++ src/main/java/edu/alonso/daw/Mandar/App.java
@@ -0,0 +1,13 @@
+package edu.alonso.daw.Mandar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
